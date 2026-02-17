84a4aa67596b9e2886dc5c0efe261bf0462b0b6d
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-02-17 11:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn=document.querySelect("input[type='button']")
+let select=document.getElementById("colorSelect")
+btn.EventListener("click", function(){
+	select.remove(select.selectedIndex)
+});
